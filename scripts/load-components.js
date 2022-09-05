dab8c3e27e223a75065005a66048a6729fff6376
julian-q <julianhquevedo@gmail.com>
2022-09-05 13:11:08 -0700

    random color sidebar

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,8 +1,11 @@
 $(function() {
-    $.getScript("/sketches/content/040722/sketch.js")
     $("td.icon").load("/components/icon.html");
     $("td.banner").load("/components/banner.html");
     $("td.left-column").load("/components/left-column.html");
+    let r = Math.floor(Math.random() * 127) + 128;
+    let g = Math.floor(Math.random() * 127) + 128;
+    let b = Math.floor(Math.random() * 127) + 128;
+    $("td.left-column").css("background-color", "rgb(" + r + ", " + g + ", " + b + ")");
     $("div#footer").load("/components/footer.html");
     $("td.main-column").load("content/index.html", renderMath);
 })
