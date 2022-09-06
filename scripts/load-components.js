9b7912e8330976813802d6551156a5d619f3617f
julian-q <julianhquevedo@gmail.com>
2022-09-05 22:14:06 -0700

    use tag selectors to speed up jquery load

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,7 +1,7 @@
 $(function() {
-    $("td.icon").load("/components/icon.html");
-    $("td.banner").load("/components/banner.html");
-    $("td.left-column").load("/components/left-column.html");
+    $("td#icon").load("/components/icon.html");
+    $("td#banner").load("/components/banner.html");
+    $("td#left-column").load("/components/left-column.html");
+    $("td#main-column").load("content/index.html", renderMath);
     $("div#footer").load("/components/footer.html");
-    $("td.main-column").load("content/index.html", renderMath);
 })
