5cc03e2e6ad99d1a3726d76235801b6b671c9d62
julian-q <julianhquevedo@gmail.com>
2022-09-09 22:07:22 -0700

    mobile friendly layout

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,7 +1,5 @@
 $(function() {
-    $("td#icon").load("/components/icon.html");
-    $("td#banner").load("/components/banner.html");
-    $("td#left-column").load("/components/left-column.html");
-    $("td#main-column").load("content/index.html", renderMath);
-    $("div#footer").load("/components/footer.html");
+    $("#banner").load("/components/banner.html");
+    $("#nav").load("/components/nav.html");
+    $("#content").load("content/index.html", renderMath);
 })
