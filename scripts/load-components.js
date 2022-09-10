e659775f69e1bf60eed746f908df6c08649aba15
julian-q <julianhquevedo@gmail.com>
2022-09-10 00:55:56 -0700

    FASTER load

--- scripts/load-components.js
+++ scripts/load-components.js
@@ -1,7 +1,6 @@
 $(function() {
-    $("#banner").load("/components/banner.html");
-    $("#nav").load("/components/nav.html");
-    $("#content").load("content/main.html", renderMath);
+    $("div#nav").load("/components/nav.html");
+    $("div#content").load("content/main.html", renderMath);
     $.get("content/main.md", function(data) {
         if (data.charAt(0) == '#') {
             let endOfTitle = data.indexOf('\n');
